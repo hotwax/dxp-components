e0959bd15262130fa1aecb8e2fdbc08eb458764a
Disha <dishatalreja1202@gmail.com>
2021-11-24 19:04:07 +0530

    Updated code to use ion-modal for barcode and qrcode scanner

---
+++ src/views/Scanner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="scanner">
+        <StreamBarcodeReader
+            @decode="onDecode"
+            @loaded="onLoaded"
+        />
+    </div> 
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+export default {
+    name: 'Scanner',
+    components:{
+        StreamBarcodeReader,
+    }
+}
+</script>
