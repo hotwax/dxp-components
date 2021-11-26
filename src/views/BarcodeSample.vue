3b8253ed9a09413d005dcc0640b3f1ecf33140c9
Disha <dishatalreja1202@gmail.com>
2021-11-26 14:27:57 +0530

    Removed a log statement and fixed indentation isseue

---
+++ src/views/BarcodeSample.vue
@@ -0,0 +1,11 @@
+<template>
+  <vue-barcode  value="123456734567" />
+</template>
+<script>
+import VueBarcode from '@chenfengyuan/vue-barcode';
+export default {
+  components: {
+    VueBarcode
+  }
+}
+</script>
