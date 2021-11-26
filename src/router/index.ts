2e95949fa6fd0ada70cd3a84e6a7ca32f700b59f
Disha <dishatalreja1202@gmail.com>
2021-11-26 19:06:22 +0530

    Added a close button to dismiss scanner modal

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import { RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Settings from "@/views/Settings.vue"
-import BarcodeSample from "@/views/BarcodeSample.vue"
-import QRcodeSample from "@/views/QRcodeSample.vue"
 import BarcodeQRcodeScanner from "@/views/BarcodeQRcodeScanner.vue"
 import store from '@/store'
 
@@ -46,14 +44,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Login',
     component: Login,
     beforeEnter: loginGuard
-  },
-  {
-    path: '/barcode',
-    component: BarcodeSample
-  },
-  {
-    path: '/qrcode',
-    component: QRcodeSample
   },
   {
     path: '/scanner',
--- src/views/Scanner.vue
+++ src/views/Scanner.vue
@@ -1,4 +1,11 @@
 <template>
+  <ion-toolbar>
+    <ion-buttons slot="end" @click="closeScanner()" >
+      <ion-button >
+        <ion-icon :icon="closeOutline" />
+      </ion-button>
+    </ion-buttons>  
+  </ion-toolbar>   
   <div class="scanner">
     <StreamBarcodeReader
       @decode="onDecode"
@@ -10,6 +17,9 @@
 <script>
 import { StreamBarcodeReader } from "vue-barcode-reader";
 import { modalController } from '@ionic/vue';
+import { 
+  closeOutline
+} from 'ionicons/icons';
 export default {
   name: 'Scanner',
   components: {
@@ -17,7 +27,15 @@ export default {
   },
   methods: {
     onDecode (result) {
+      this.closeScanner();
+    },
+    closeScanner(){
       modalController.dismiss({dismissed: true});
+    }
+  },
+  setup() {
+    return {
+      closeOutline
     }
   }
 }
