c365c21832fe19f1b3f06955f576e80934c458a6
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-12-01 18:00:04 +0530

    Improved comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,24 +18,22 @@ const showToast = async (message: string) => {
   return toast.present();
 }
 
-
-
 // Utility for parsing CSV file 
 // Package Used : PapaParse (Link to Documentation : https://www.papaparse.com/docs#config)
 
-// In this we will be recieving the file and options in the function 
-// and we returning a promise with results in it 
+// In this we will be receiving the file and options in the function 
+// and we are returning a promise with results in it 
 
-// We have used the parse method of the Papa utility which will take a config object with File.
+// We have used the parse method of the papaparse library which will take a config object with File.
 // In the config object we have passed various keys:
-//   - header : It tells Papaparse that there will be a header in the CSV. 
+//   - header : It tells papaparse that there will be a header in the CSV. 
 //   - skipEmptyLines : It will ignore any empty lines in the CSV.
 //   - complete : A parse result always contains three objects: data, errors, and meta. 
-//     Data and errors are arrays, and meta is an object. In the step callback, the data 
+//     data and errors are arrays, and meta is an object. In the step callback, the data 
 //     array will only contain one element.
 
 // Also, we have passed options, as if user wants to add some more properties to the method 
-// or if he want to modify some pre build keys then he can do so.
+// or if he want to modify some pre-build keys then he can do so.
 
 // Types of Responses
 
@@ -45,7 +43,6 @@ const showToast = async (message: string) => {
 // "Mike",5,b
 // "Jake",33,c
 // "Jill",30,d
-
 
 // For (header:true) we get
 // [{columnA: 'Susan', columnB: '41', columnC: 'a'},
@@ -59,8 +56,6 @@ const showToast = async (message: string) => {
 // ['Mike', '5', 'b'],
 // ['Jake', '33', 'c'],
 // ['Jill', '30', 'd']]
-
-
 
 const parseCsv = async (file: File, options: any) => {
   return new Promise ((resolve, reject) => {
