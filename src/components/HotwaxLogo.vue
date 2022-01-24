9ee3160fe4e8352165450db964aa3eebbc2bc27b
Azkya Khan <azkya.khan@hotwax.co>
2022-01-24 21:30:30 +0530

    Created Hotwax logo component (#1xr7yz3)

---
+++ src/components/HotwaxLogo.vue
@@ -0,0 +1,34 @@
+<template>
+  <figure>
+    <img class="light" src="../assets/images/hc.png"/>
+    <img class="dark" src="../assets/images/HWCLogoDarkMode.png"/>
+    </figure>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "HotwaxLogo",
+})
+</script>
+
+<style scoped>
+.dark {
+  display: none;
+}
+
+.light {
+  display: unset;
+}
+
+@media (prefers-color-scheme: dark) {
+  .dark {
+    display: unset;
+  }
+  
+  .light {
+    display: none;
+  }
+}
+</style>
