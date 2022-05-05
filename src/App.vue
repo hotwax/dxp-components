5b09587c01b1e35432f781da61a7c1463f12f799
niharika <niharika2nas@gmail.com>
2022-05-05 16:46:25 +0530

    Removed timezone mismatch alert on login(#2893rqm)

--- src/App.vue
+++ src/App.vue
@@ -22,28 +22,6 @@ export default defineComponent({
     }
   },
   methods: {
-    async timeZoneDifferentAlert(payload: any) {
-      const alert = await alertController.create({
-        header: this.$t("Change time zone"),
-        message: this.$t('Would you like to update your time zone to . Your profile is currently set to . This setting can always be changed from the settings menu.', { localTimeZone: payload.localTimeZone, profileTimeZone: payload.profileTimeZone }),
-        buttons: [
-            {
-              text: this.$t("Dismiss"),
-              role: 'cancel',
-              cssClass: 'secondary'
-            },
-            {
-              text: this.$t("Update time zone"),
-              handler: () => {
-                this.store.dispatch("user/setUserTimeZone", {
-                    "tzId": payload.localTimeZone
-                });
-              },
-            },
-          ],
-      });
-      return alert.present();
-    },
     async presentLoader() {
       if (!this.loader) {
         this.loader = await loadingController
@@ -69,12 +47,10 @@ export default defineComponent({
         translucent: true,
         backdropDismiss: true
       });
-    emitter.on('timeZoneDifferent', this.timeZoneDifferentAlert);
     emitter.on('presentLoader', this.presentLoader);
     emitter.on('dismissLoader', this.dismissLoader);
   },
   unmounted() {
-    emitter.off('timeZoneDifferent', this.timeZoneDifferentAlert);
     emitter.off('presentLoader', this.presentLoader);
     emitter.off('dismissLoader', this.dismissLoader);
   },
