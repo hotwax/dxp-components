49d45772e01d6b152a3077e7465c271e31ede62d
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-06-10 16:50:44 +0530

    Implemented: shipment schema for apps taking reference from moqui(#2f88u9r)

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+import { ContactMech } from "./ContactMech";
+import { Enumeration } from "./Enumeration";
+import { Geo } from "./Geo";
+import { Identification } from "./Identification";
+import { Party } from "./Party";
+import { Shipment } from "./Shipment";
+import { Status } from "./Status";
+import { Uom } from "./Uom";
+
+export {
+  ContactMech,
+  Enumeration,
+  Geo,
+  Identification,
+  Party,
+  Shipment,
+  Status,
+  Uom
+}
