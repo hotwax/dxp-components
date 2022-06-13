804475b8b0b768990f2f78f17729891fc39175c5
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-06-13 13:08:58 +0530

    Implemented: inventory schema taking reference from the moqui asset schema(#2fdy3m2)

---
+++ src/types/Geo.ts
@@ -0,0 +1,10 @@
+export interface Geo {
+  geoId: string,
+  geoTypeEnumId?: string,
+  geoName?: string,
+  geoNameLocal?: string,
+  geoCodeAlpha2?: string,
+  geoCodeAlpha3?: string,
+  geoCodeNumeric?: string,
+  wellKnownText?: string,
+}
---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { ContactMech } from "./ContactMech";
+import { Enumeration } from "./Enumeration";
+import { Geo } from "./Geo";
+import { Identification } from "./Identification";
+import { Party } from "./Party";
+import { Status } from "./Status";
+
+export {
+  ContactMech,
+  Enumeration,
+  Geo,
+  Identification,
+  Party,
+  Status
+}
