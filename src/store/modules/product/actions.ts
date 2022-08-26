93947f2adbe3e4295a8d2aaf3782f27505a201eb
Yash Maheshwari <yashmaheshwari551@gmail.com>
2022-08-26 15:53:12 +0530

    Implemented: support for oms api package and updated the code to set the token and instance url(#2rby5m5)

--- src/store/modules/product/actions.ts
+++ src/store/modules/product/actions.ts
@@ -1,11 +1,11 @@
-import { ProductService } from "@/services/ProductService";
 import { ActionTree } from 'vuex'
 import RootState from '@/store/RootState'
 import ProductState from './ProductState'
 import * as types from './mutation-types'
-import { hasError, showToast } from '@/utils'
+import { showToast } from '@/utils'
 import { translate } from '@/i18n'
 import emitter from '@/event-bus'
+import { fetchProducts, isError } from "@hotwax/oms-api";
 
 
 const actions: ActionTree<ProductState, RootState> = {
@@ -19,17 +19,18 @@ const actions: ActionTree<ProductState, RootState> = {
     let resp;
 
     try {
-      resp = await ProductService.fetchProducts({
+      resp = await fetchProducts({
         // used sku as we are currently only using sku to search for the product
-        "filters": ['sku: ' + payload.queryString],
+        "queryString": payload.queryString,
+        "queryFields": "sku",
         "viewSize": payload.viewSize,
         "viewIndex": payload.viewIndex
       })
 
       // resp.data.response.numFound tells the number of items in the response
-      if (resp.status === 200 && resp.data.response.numFound > 0 && !hasError(resp)) {
-        let products = resp.data.response.docs;
-        const totalProductsCount = resp.data.response.numFound;
+      if (!isError(resp) && resp.total > 0) {
+        let products = resp.products;
+        const totalProductsCount = resp.total;
 
         if (payload.viewIndex && payload.viewIndex > 0) products = state.products.list.concat(products)
         commit(types.PRODUCT_SEARCH_UPDATED, { products: products, totalProductsCount: totalProductsCount })
