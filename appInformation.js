c042b68750c09c1acd0bf1c89cb5c681071041d5
Aditya Sharma <aditya.sharma@hotwax.co>
2023-01-05 18:24:06 +0530

    Improved script to show tag, branch and revision information

--- appInformation.js
+++ appInformation.js
@@ -1,7 +1,45 @@
+const { execSync } = require('child_process');
+const package = require('./package.json');
+
 (function() {
-    const appInformation = {
-        version: "1.2.0"
+    const appVersionInfo = {
+        version: "",
+        branch: "",
+        tag: "",
+        revision: "",
+        builtTime: ""
     };
-    process.env.VUE_APP_INFORMATION = JSON.stringify(appInformation);
-    return appInformation;
+
+    try {
+        // Extract package version
+        appVersionInfo.version = package.version;
+    
+        // Extract build time
+        appVersionInfo.builtTime = Date.now();
+
+        // Extract branch
+        let output = executeCommand('git symbolic-ref --short -q HEAD');
+        appVersionInfo.branch = output;
+
+        // Extract tag
+        // 2> /dev/null
+        // 2> redirects stderr to file
+        // /dev/null null device takes any input and throws away
+        output = executeCommand('git describe --tags --exact-match 2> /dev/null');
+        appVersionInfo.tag = output;
+
+        // Extract revision
+        output = executeCommand('git rev-parse --short HEAD');
+        appVersionInfo.revision = output;
+    } catch (err){ 
+        console.warn("App version info package error: ", err.toString())
+    }
+    // TODO make it a generic plugin and separate out Vue specific environment variable setup
+    process.env.VUE_APP_VERSION_INFO = JSON.stringify(appVersionInfo);
+
+    function executeCommand(command) {
+      // TODO Handle error cases
+      // Check if should use spawn instead 
+      return execSync(command).toString();
+    }
 })();
