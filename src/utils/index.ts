569aa83e203d4eda0682e3e36cbedc868ba4a188
k2maan <ketuman333@gmail.com>
2023-07-05 14:35:18 +0530

    Added: additional parameters in window.open function (#99)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 const goToOms = (token: string, oms: string) => {
   const link = (oms.startsWith('http') ? oms.replace('api/', "") : `https://${oms}.hotwax.io/`) + `?token=${token}`
-  window.open(link)
+  window.open(link, '_blank', 'noopener, noreferrer')
 }
 
 export {
