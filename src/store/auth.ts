213f83fa0a2c7553b2856f7b69a9319a31f9303b
k2maan <ketuman333@gmail.com>
2023-07-12 11:15:26 +0530

    Updated: logic for isAuthenticated getter and removed unnecessary code

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,13 +1,12 @@
-import { UserService } from "../services/UserService";
 import { defineStore } from "pinia";
-import { hasError } from "@hotwax/oms-api";
+import { DateTime } from 'luxon'
 
 export const useAuthStore = defineStore('userAuth', {
   state: () => {
     return {
       token: {
         value: '',
-        expiry: null
+        expiration: undefined
       },
       oms: 'dev-oms'
     }
@@ -15,31 +14,20 @@ export const useAuthStore = defineStore('userAuth', {
   getters: {
     getToken: (state) => state.token,
     getOms: (state) => state.oms,
-    isAuthenticated: (state) => state.token.value.length > 0
+    isAuthenticated: (state) => {
+      let isTokenExpired = false
+      if (state.token.expiration) {
+        const currTime = DateTime.now().toMillis()
+        isTokenExpired = state.token.expiration < currTime
+      }
+      return state.token.value && !isTokenExpired
+    }
   },
   actions: {
-    /**
-     * For authentication in route guard
-     */
 		async authenticate() {
       try {
         // authenticate through cookies
         this.token.value = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6ImhvdHdheC51c2VyIiwiaXNzIjoiSG90V2F4IiwiZXhwIjoxNjg4NDY4NTU3LCJpYXQiOjE2ODgzODIxNTd9.czfRthWsv82F4V9Uq79u3NJ1ziOiME8nlO4y0ITSkvugBxA15jVWqd0q8Grh86Utt2qX6T3-vUEDlivKJ6b6vw'
-        return this.token
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    /**
-     * For login using username and password through launchpad
-     */
-    async login() {
-      try {
-        // get token, oms, etc from query and
-        // push them into the route from launchpad
-        const resp = await UserService.login('aaron.wagner', 'hotwax@786')
-        // set token and auth data
-        if (!hasError(resp)) this.token.value = resp.data.token
         return this.token
       } catch (error) {
         console.log(error)
