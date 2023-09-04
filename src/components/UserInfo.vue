21302be3d5b5d6e099c3e85c1d0c73815443f83b
Yash Maheshwari <yashmaheshwari551@gmail.com>
2023-09-04 12:22:16 +0530

    Implemented: support to use ionic components in dxp components(#95)

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-card>
+    <ion-item lines="full">
+      <ion-avatar slot="start" v-if="user?.partyImageUrl">
+        <Image :src="user.partyImageUrl"/>
+      </ion-avatar>
+      <!-- ion-no-padding to remove extra side/horizontal padding as additional padding 
+      is added on sides from ion-item and ion-padding-vertical to compensate the removed
+      vertical padding -->
+      <ion-card-header class="ion-no-padding ion-padding-vertical">
+        <ion-card-subtitle>{{ user?.userLoginId }}</ion-card-subtitle>
+        <ion-card-title>{{ user?.partyName }}</ion-card-title>
+      </ion-card-header>
+    </ion-item>
+    <ion-button color="danger">{{ "Logout" }}</ion-button>
+    <ion-button fill="outline">
+      {{ "Go to Launchpad" }}
+    </ion-button>
+  </ion-card>
+</template>
+
+<script setup lang="ts">
+defineProps(["user"])
+</script>
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Login from "./components/Login";
 import ShopifyImg from "./components/ShopifyImg";
 import { goToOms } from "./utils";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { UserInfo } from "./components";
 
 // TODO: handle cases when the store from app or pinia store are not available
 // creating a pinia store for the plugin
@@ -26,6 +27,7 @@ export let dxpComponents = {
 
     app.component('Login', Login)
     app.component('ShopifyImg', ShopifyImg)
+    app.component('UserInfo', UserInfo)
 
     loginContext.login = options.login
     loginContext.logout = options.logout
@@ -47,5 +49,6 @@ export {
   ShopifyImg,
   goToOms,
   appContext,
-  productIdentificationContext
+  productIdentificationContext,
+  UserInfo
 }
