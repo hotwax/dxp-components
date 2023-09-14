700c9d1f6d89708a0ff5d6a0f8449e3a7f3e288a
k2maan <ketuman333@gmail.com>
2023-09-14 16:06:21 +0530

    Implemented: firebase app intitialisation through DXP on login

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { createPinia } from "pinia";
 import { useProductIdentificationStore } from "./store/productIdentification";
 import { useAuthStore } from "./store/auth";
+
 import Login from "./components/Login";
 import ShopifyImg from "./components/ShopifyImg";
 import { goToOms } from "./utils";
+import { initialiseFirebaseApp } from "./utils/firebase"
+
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 // TODO: handle cases when the store from app or pinia store are not available
@@ -15,6 +18,7 @@ let loginContext = {} as any
 let shopifyImgContext = {} as any
 let appContext = {} as any
 let productIdentificationContext = {} as any
+let noitificationContext = {} as any
 
 // executed on app initialization
 export let dxpComponents = {
@@ -35,19 +39,27 @@ export let dxpComponents = {
     shopifyImgContext.defaultImgUrl = options.defaultImgUrl
     productIdentificationContext.getProductIdentificationPref = options.getProductIdentificationPref
     productIdentificationContext.setProductIdentificationPref = options.setProductIdentificationPref
+    
+    noitificationContext.addNotification = options.addNotification
+    noitificationContext.appFirebaseConfig = options.appFirebaseConfig
+    noitificationContext.appFirebaseVapidKey = options.appFirebaseVapidKey
+    noitificationContext.storeClientRegistrationToken = options.storeClientRegistrationToken
+
     loginContext.getConfig = options.getConfig
     loginContext.initialise = options.initialise
   }
 }
 
 export {
-  useProductIdentificationStore,
-  useAuthStore,
+  appContext,
+  goToOms,
+  initialiseFirebaseApp,
   Login,
   loginContext,
-  shopifyImgContext,
+  productIdentificationContext,
+  noitificationContext,
   ShopifyImg,
-  goToOms,
-  appContext,
-  productIdentificationContext
+  shopifyImgContext,
+  useProductIdentificationStore,
+  useAuthStore,
 }
