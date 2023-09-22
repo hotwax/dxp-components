6d637e51a763e91def1f127d0b3ee2f24b639918
Sanskar Soni <sonisanskar223@gmail.com>
2023-09-22 11:36:47 +0530

    Improved: computed property name and removed unwanted condition in ion select option (#126)

--- src/components/MenuFooterNavigation.vue
+++ src/components/MenuFooterNavigation.vue
@@ -5,31 +5,31 @@
         <ion-label class="ion-text-wrap">
           <p class="overline">{{ instanceUrl }}</p>
         </ion-label>
-        <ion-note slot="end">{{ appUserState.userProfile?.userTimeZone }}</ion-note>
+        <ion-note slot="end">{{ userAppState.userProfile?.userTimeZone }}</ion-note>
       </ion-item>
       <!-- showing product stores only when there are multiple options to choose from. -->
-      <ion-item v-if="appUserState.userProfile?.stores?.length > 2" lines="none">
+      <ion-item v-if="userAppState.userProfile?.stores?.length > 2" lines="none">
         <!-- WHY EVENTS ($emit) IS USED WITH ION CHANGE: https://michaelnthiessen.com/pass-function-as-prop/ -->
-        <ion-select interface="popover" :value="appUserState.currentEComStore.productStoreId" @ionChange="$emit('updateEcomStore', $event)">
-          <ion-select-option v-for="store in (appUserState.userProfile?.stores ? appUserState.userProfile.stores : [])" :key="store.productStoreId" :value="store.productStoreId">{{ store.storeName }}</ion-select-option>
+        <ion-select interface="popover" :value="userAppState.currentEComStore.productStoreId" @ionChange="$emit('updateEcomStore', $event)">
+          <ion-select-option v-for="store in userAppState.userProfile.stores" :key="store.productStoreId" :value="store.productStoreId">{{ store.storeName }}</ion-select-option>
         </ion-select>
       </ion-item>
       <ion-item v-else lines="none">
         <ion-label class="ion-text-wrap">
-          {{ appUserState.currentEComStore.storeName }}
+          {{ userAppState.currentEComStore.storeName }}
         </ion-label>
       </ion-item>
       <!-- similarly, showing shopify configs only when there are multiple options to choose from 
       but if both product store and config have multiple options, then only option to choose
       product store will be visible -->
-      <ion-item v-if="appUserState.shopifyConfigs?.length > 1 && appUserState.userProfile?.stores?.length < 3" lines="none">
-        <ion-select interface="popover" :value="appUserState.currentShopifyConfig?.shopifyConfigId" @ionChange="$emit('updateShopifyConfig', $event)">
-          <ion-select-option v-for="shopifyConfig in appUserState.shopifyConfigs" :key="shopifyConfig.shopifyConfigId" :value="shopifyConfig.shopifyConfigId">{{ shopifyConfig.name ? shopifyConfig.name : shopifyConfig.shopifyConfigName }}</ion-select-option>
+      <ion-item v-if="userAppState.shopifyConfigs?.length > 1 && userAppState.userProfile?.stores?.length < 3" lines="none">
+        <ion-select interface="popover" :value="userAppState.currentShopifyConfig?.shopifyConfigId" @ionChange="$emit('updateShopifyConfig', $event)">
+          <ion-select-option v-for="shopifyConfig in userAppState.shopifyConfigs" :key="shopifyConfig.shopifyConfigId" :value="shopifyConfig.shopifyConfigId">{{ shopifyConfig.name ? shopifyConfig.name : shopifyConfig.shopifyConfigName }}</ion-select-option>
         </ion-select>
       </ion-item>
       <ion-item v-else lines="none">
         <ion-label class="ion-text-wrap">
-          <p>{{ appUserState.currentShopifyConfig.name ? appUserState.currentShopifyConfig.name : appUserState.currentShopifyConfig.shopifyConfigName }}</p>
+          <p>{{ userAppState.currentShopifyConfig.name ? userAppState.currentShopifyConfig.name : userAppState.currentShopifyConfig.shopifyConfigName }}</p>
         </ion-label>
       </ion-item>
     </ion-toolbar>
@@ -44,7 +44,7 @@ import { computed } from 'vue';
 const authStore = useAuthStore();
 const appState = appContext.config.globalProperties.$store;
 const instanceUrl = computed(() => authStore.getOms); 
-const appUserState = computed(() => {
+const userAppState = computed(() => {
   return {
     userProfile: appState.getters['user/getUserProfile'],
     currentEComStore: appState.getters['user/getCurrentEComStore'],
