10595c710b1000d5810adf4beacc84a5decc607f
amansinghbais <singh11amanbais@gmail.com>
2023-09-25 12:13:59 +0530

    Improved: code to use deep-clone for the computed properties, method name's (#138)

--- src/components/FacilitySwitcher.vue
+++ src/components/FacilitySwitcher.vue
@@ -2,50 +2,61 @@
   <ion-card>
     <ion-card-header>
       <ion-card-title>
-        {{ "Facility" }}
+        {{ $t('Facility') }}
       </ion-card-title>
     </ion-card-header>
     <ion-card-content>
-      {{ 'Specify which facility you want to operate from. Order, inventory and other configuration data will be specific to the facility you select.' }}
+      {{ $t('Specify which facility you want to operate from. Order, inventory and other configuration data will be specific to the facility you select.') }}
     </ion-card-content>
     <ion-item lines="none">
-      <ion-label>{{ "Select facility" }}</ion-label>
-      <ion-select interface="popover" :value="appUserState.currentFacility.facilityId" @ionChange="setFacility($event)">
-        <ion-select-option v-for="facility in (appUserState.userProfile ? appUserState.userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.name }}</ion-select-option>
+      <ion-label>{{ $t('Select facility') }}</ion-label>
+      <ion-select interface="popover" :value="userAppState.currentFacility.facilityId" @ionChange="setFacility($event)">
+        <ion-select-option v-for="facility in (userAppState.userProfile ? userAppState.userProfile.facilities : [])" :key="facility.facilityId" :value="facility.facilityId" >{{ facility.facilityName ? facility.facilityName : facility.name }}</ion-select-option>
       </ion-select>
     </ion-item>
   </ion-card>
 </template>
 
 <script setup lang="ts">
-import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonItem, IonLabel, IonSelect, IonSelectOption } from '@ionic/vue'
+import { 
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption
+} from '@ionic/vue';
 import { appContext } from '../index';
 import { computed } from 'vue';
 
 // Here 'facility-change-alert' and 'update-facility-id' are the events for inventory-count app.
 // and 'get-facility-details' is the event for fulfillment-pwa app.
 const emit = defineEmits(['facility-change-alert', 'get-facility-details' ,'update-facility-id'])
-const store = appContext.config.globalProperties.$store;
-const appUserState = computed(() => {
+const appState = appContext.config.globalProperties.$store;
+
+const userAppState = computed(() => {
   return {
-    currentFacility: store.getters['user/getCurrentFacility'],
-    userProfile: store.getters['user/getUserProfile'],
-    uploadProducts: store.getters['product/getUploadProducts']
+    currentFacility: appState.getters['user/getCurrentFacility'],
+    userProfile: appState.getters['user/getUserProfile'],
+    uploadProducts: appState.getters['product/getUploadProducts']
   }
 });
 
 const setFacility = async (event: any) => {
-  const currentAppUserState = appUserState.value
+  const currentUserAppState = JSON.parse(JSON.stringify(userAppState.value))
   const selectedFacility = event['detail'].value
-  if (currentAppUserState.currentFacility.facilityId && currentAppUserState.currentFacility.facilityId != selectedFacility && currentAppUserState.userProfile?.facilities) {
+
+  if(currentUserAppState.currentFacility.facilityId && currentUserAppState.currentFacility.facilityId != selectedFacility && currentUserAppState.userProfile?.facilities) {
     // Below check is a handle case for the inventory-count app.
-    if (store.uploadProducts && Object.keys(store.uploadProducts).length > 0 ) {
+    if(appState.uploadProducts && Object.keys(appState.uploadProducts).length > 0 ) {
       emit('facility-change-alert', selectedFacility)
     } else {
-      await store.dispatch('user/setFacility', {
-        'facility': currentAppUserState.userProfile.facilities.find((fac: any) => fac.facilityId == selectedFacility)
+      await appState.dispatch('user/setFacility', {
+        'facility': currentUserAppState.userProfile.facilities.find((fac: any) => fac.facilityId == selectedFacility)
       });
-      Promise.all([emit('update-facility-id', currentAppUserState.currentFacility.facilityId), emit('get-facility-details')])
+      Promise.all([emit('update-facility-id', currentUserAppState.currentFacility.facilityId), emit('get-facility-details')])
     }
   }
 }
