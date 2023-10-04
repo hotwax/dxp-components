7fddd5e54227a177bfd384d1f67036597c126105
k2maan <ketuman333@gmail.com>
2023-10-04 15:07:57 +0530

    Implemented: locale persistence as a user perference (#196)

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,24 +1,37 @@
 import { defineStore } from "pinia";
 import { i18n } from "../../src";
+import { userContext } from "../../src";
 
 declare let process: any;
 
 export const useUserStore = defineStore('user', {
   state: () => {
     return {
-      locale: 'en',
-      localeOptions: process.env.VUE_APP_LOCALES ? JSON.parse(process.env.VUE_APP_LOCALES) : { "en": "English" }
+      localeOptions: process.env.VUE_APP_LOCALES ? JSON.parse(process.env.VUE_APP_LOCALES) : { "en": "English" },
+      preference: {
+        locale: 'en'
+      } as any
     }
   },
   getters: {
-    getLocale: (state) => state.locale,
+    getLocale: (state) => state.preference.locale,
     getLocaleOptions: (state) => state.localeOptions
   },
   actions: {
     setLocale(payload: string) {
       // update locale in state and globally
       i18n.global.locale.value = payload
-      this.locale = payload
+      this.setPreference({ locale: payload })
+    },
+    async setPreference(payload: any) {
+      this.preference = { ...this.preference, ...payload }
+      await userContext.setUserPreference({
+        'userPrefTypeId': 'LOCALE_PREFERENCE',
+        'userPrefValue': JSON.stringify(this.preference)
+      })
+    },
+    async getPreference(token: any, baseURL: string) {
+      this.preference = await userContext.getUserPreference(token, baseURL, 'LOCALE_PREFERENCE')
     }
   },
   persist: true
