30c28d4398b0cf38a4c4fbbcda7e796be384795d
amansinghbais <singh11amanbais@gmail.com>
2023-10-12 16:01:18 +0530

    Improved: variable name and used arrow function in computed(#138)

--- src/components/DxpFacilitySwitcher.vue
+++ src/components/DxpFacilitySwitcher.vue
@@ -34,28 +34,25 @@ import { computed } from 'vue';
 const emit = defineEmits(['before-set-facility', 'after-set-facility']);
 const appState = appContext.config.globalProperties.$store;
 
-const userAppState = computed(() => {
-  return {
-    currentFacility: appState.getters['user/getCurrentFacility'],
-    userProfile: appState.getters['user/getUserProfile'],
-    uploadProducts: appState.getters['product/getUploadProducts']
-  }
-});
+const userAppState = computed(() => ({
+  currentFacility: appState.getters['user/getCurrentFacility'],
+  userProfile: appState.getters['user/getUserProfile']
+}));
 
 const setFacility = async (event: CustomEvent) => {
   const currentUserAppState = JSON.parse(JSON.stringify(userAppState.value))
-  const selectedFacility = event.detail.value
+  const facilityId = event.detail.value
 
-  if(currentUserAppState.currentFacility.facilityId !== selectedFacility && currentUserAppState.userProfile?.facilities) {
+  if(currentUserAppState.currentFacility.facilityId !== facilityId && currentUserAppState.userProfile?.facilities) {
     // before-set-facility is emitted before setFacility action.
-    emit('before-set-facility', selectedFacility)
+    emit('before-set-facility', facilityId)
 
     await appState.dispatch('user/setFacility', {
-      'facility': currentUserAppState.userProfile.facilities.find((facility: any) => facility.facilityId === selectedFacility)
+      'facility': currentUserAppState.userProfile.facilities.find((facility: any) => facility.facilityId === facilityId)
     });
 
     // after-set-facility is emitted after setFacility action.
-    emit('after-set-facility', selectedFacility)
+    emit('after-set-facility', facilityId)
   }
 }
 </script>
