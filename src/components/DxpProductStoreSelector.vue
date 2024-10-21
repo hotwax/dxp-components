a920ed6e8afecdf1b4f9e45d99a900c04dfd4567
R-Sourabh <rsourabh002@gmail.com>
2024-10-21 11:17:12 +0530

    Improved: Updated @ionChange handler to use ($event.detail.value) instead of $event(#193)

--- src/components/DxpProductStoreSelector.vue
+++ src/components/DxpProductStoreSelector.vue
@@ -14,7 +14,7 @@
     </ion-card-content>
 
     <ion-item lines="none">
-      <ion-select :label="$t('Select store')" interface="popover" :placeholder="$t('store name')" :value="currentEComStore?.productStoreId" @ionChange="updateEComStore($event)">
+      <ion-select :label="$t('Select store')" interface="popover" :placeholder="$t('store name')" :value="currentEComStore?.productStoreId" @ionChange="updateEComStore($event.target.value)">
         <ion-select-option v-for="store in (eComStores ? eComStores : [])" :key="store.productStoreId" :value="store.productStoreId">{{ store.storeName }}</ion-select-option>
       </ion-select>
     </ion-item>
@@ -32,9 +32,9 @@ const emit = defineEmits(["updateEcomStore"])
 const eComStores = computed(() => userStore.getProductStores); 
 const currentEComStore = computed(() => userStore.getCurrentEComStore);
 
-async function updateEComStore(event: any) {
-  if (event.target.value && currentEComStore.value?.productStoreId !== event.detail.value) {
-    const selectedProductStore = eComStores.value.find((store: any) => store.productStoreId == event.detail.value)
+async function updateEComStore(eComStoreId: any) {
+  if (eComStoreId && currentEComStore.value?.productStoreId !== eComStoreId) {
+    const selectedProductStore = eComStores.value.find((store: any) => store.productStoreId == eComStoreId)
     await userStore.setEComStorePreference(selectedProductStore)
     emit('updateEcomStore', selectedProductStore)
   }
